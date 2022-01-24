34a2e345b4f170823cce5ef8420769e5bacbc7a3
Mai Ngoc Gia Bao <maingocgiabao@gmail.com>
2022-01-24 12:44:28 -0800

    add js hamburger

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 jQuery(document).ready(function () {
-  $('.glitch').mousemove(function (e) {
+  $('.3d').mousemove(function (e) {
     var rXP = (e.pageX - this.offsetLeft - $(this).width() / 2);
     var rYP = (e.pageY - this.offsetTop - $(this).height() / 2);
-    $('.glitch').css('text-shadow', +rYP / 10 + 'px ' + rXP / 80 + 'px rgba(227,6,19,.8), ' + rYP / 8 + 'px ' + rXP / 60 + 'px rgba(255,237,0,1), ' + rXP / 70 + 'px ' + rYP / 12 + 'px rgba(0,159,227,.7)');
+    $('.3d').css('text-shadow', +rYP / 10 + 'px ' + rXP / 80 + 'px rgba(227,6,19,.8), ' + rYP / 8 + 'px ' + rXP / 60 + 'px rgba(255,237,0,1), ' + rXP / 70 + 'px ' + rYP / 12 + 'px rgba(0,159,227,.7)');
   });
-  console.log('scroll')
+
+  let hamburger = $('.hamburger')
+  hamburger.click(function () {
+    $('nav').toggleClass("open-nav");
+    $('.hamburger .bar1').toggleClass("change1");
+    $('.hamburger .bar2').toggleClass("change2");
+    $('.hamburger .bar3').toggleClass("change3");
+  })
 });
